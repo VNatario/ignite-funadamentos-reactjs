b90645a10f59832b60f255958c051c8a43b0f1e7
Vitor Natario <natariovitor@gmail.com>
2022-08-22 11:54:03 -0300

    feat: create header with module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import "./styles.css";
 
 export function App() {
   return (
     <>
+      <Header />
+
       <Post
         author="Natario"
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit enim optio iusto ipsam eligendi perspiciatis quia tempora debitis nemo, asperiores alias totam ab. Debitis, laborum ut? Error asperiores expedita adipisci!"
