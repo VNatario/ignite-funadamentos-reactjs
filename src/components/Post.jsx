e16d8e8a38ff7732feedca1eac9f5547fbf1678d
Vitor Natario <natariovitor@gmail.com>
2022-08-23 19:38:50 -0300

    feat: creating avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import styles from "./Post.module.css";
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 
 export function Post() {
@@ -6,10 +7,8 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/VNatario.png"
-          />
+          <Avatar src="https://github.com/SaraAnita1.png" />
+
           <div className={styles.authorInfo}>
             <strong>Vitor Natario</strong>
             <span>Web Developer</span>
